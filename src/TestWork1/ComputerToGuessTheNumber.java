d9b3754ab6eebce4141e9257adc40b5b0d4a38d7
Lena <bunechka@inbox.lv>
2020-09-27 15:00:27 +0200

    training to git

--- src/TestWork1/ComputerToGuessTheNumber.java
+++ src/TestWork1/ComputerToGuessTheNumber.java
@@ -12,11 +12,43 @@ public class ComputerToGuessTheNumber {
     public static void main(String[] args) {
 
         Scanner CompGuessing = new Scanner(System.in);
-        int guess = 1 + (int) (100 * Math.random());
+        System.out.println("Think of a whole number between 1 and 100. I'll guess what it is!");
+        System.out.println("When you're ready, type yes and press enter.");
 
-        System.out.println("Guessing number is: " + guess);
-        System.out.println("My number is bigger than " + guess);
-        System.out.println("My number is smaller than " + guess);
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
 
+        int min = 1;
+        int max = 100;
+
+        int guesses = 0;
+        boolean done = false;
+        while(!done){
+
+            int guess = min + (int)(Math.random() * (max - min + 1));
+            guesses++;
+
+            System.out.println("My guess is: " + guess);
+            System.out.println("Please type 0 if I got it right.");
+            System.out.println("Please type 1 if your number is higher.");
+            System.out.println("Please type 2 if your number is lower.");
+            System.out.println("Then press enter.");
+
+            String answer = scanner.nextLine();
+
+            if(answer.equals("2")){
+                max = guess - 1;
+            }
+            else if(answer.equals("1")){
+                min = guess + 1;
+            }
+            else{
+                System.out.println("Hooray!");
+                System.out.println("It took me " + guesses + " guesses to get it right. Thanks for playing!");
+                done = true;
+            }
+        }
+        scanner.close();
     }
 }
+
