3f1c6a736d67507c66926d290a171ee2408cef0e
Lena <bunechka@inbox.lv>
2020-09-27 16:13:21 +0200

    training to git

--- src/TestWork1/TakeTheMoney.java
+++ src/TestWork1/TakeTheMoney.java
@@ -2,25 +2,28 @@ package TestWork1;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.lang.Math;
 
 public class TakeTheMoney {
-    //Enter the amount - the program must calculate which coins and how many of them to give back. Example: 150 =>
-    //1 = 1 EUR coin, 1 = 0.50 EUR coin.
-    // Coins available: 2 and 1 EUR, 50,20, 10, 2, 1 cents.
-    //create 2 arrays for calculations.
+    //Enter the amount - the program must calculate which coins and how many of them to give back.
+    // Example: 150 =>
+    //  1 = 1 EUR coin, 1 = 0.50 EUR coin.
+    // Coins available: 2 and 1 EUR (100,200), 50,20, 10, 2, 1 cents.
+    //  use 2 arrays for calculations/results.
 
     public static void main(String[] args) {
-        int [] coinTypes =  {1, 2, 5, 10, 20, 50, 100, 200};
-        int [] givenCoins = {0, 0, 0,  0,  0,  0,   0,   0};
-        int amount = 0;
+        double [] coinList =  {0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, 2.00};
+        int [] givenCoins =   {   0,    0,    0,    0,    0,    0,    0,    0};
+        int k, value;
+
 
         System.out.println("Enter your amount for coin exchange and press enter.");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
 
-        System.out.println("Available coins are: " + Arrays.toString(coinTypes));
 
 
+        System.out.println("Available coins are: " + Arrays.toString(coinList));
         System.out.println("Your amount is made of coins: " + Arrays.toString(givenCoins));
 
 
